e0d95c389c8d0005371efbf666c68f87cbcd21b7
Lucas Préaux <contact@lucaspreaux.fr>
2025-12-07 00:07:35 +0100

    feat: Extract window logic to Window class

--- src/main/java/org/almostcraft/core/Engine.java
+++ src/main/java/org/almostcraft/core/Engine.java
@@ -14,10 +14,9 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.system.MemoryStack.stackPush;
-import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Engine {
-    private long window;
+    private Window window;
 
     public void run() throws IOException {
         Properties props = new Properties();
@@ -39,50 +38,19 @@ public class Engine {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
 
-        // Configure GLFW
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-        if (window == NULL) {
-            throw new RuntimeException("Failed to create the GLFW window");
-        }
+        // Create the windows
+        window = new Window(300, 300, "AlmostCraft");
+        window.centerOnScreen();
+        window.makeContextCurrent();
+        window.enableVsync();
+        window.show();
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+        glfwSetKeyCallback(window.getHandle(), (window, key, scancode, action, mods) -> {
             if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
                 glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
             }
         });
-
-        // Get the thread stack and push a new frame
-        try (MemoryStack stack = stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            ); // the stack frame is popped automatically
-
-            // Make the OpenGL context current
-            glfwMakeContextCurrent(window);
-            // Enable v-sync
-            glfwSwapInterval(1);
-
-            // Make the window visible
-            glfwShowWindow(window);
-        }
     }
 
     private void loop() {
@@ -93,10 +61,10 @@ public class Engine {
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while (!glfwWindowShouldClose(window)) {
+        while (!window.shouldClose()) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
-            glfwSwapBuffers(window); // swap the color buffers
+            window.swapBuffers(); // swap the color buffers
 
             // Poll for window events. The key callback above will only be
             // invoked during this call.
@@ -113,9 +81,7 @@ public class Engine {
     }
 
     private void cleanup() {
-        // Free the window callbacks and destroy the window
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
+        window.destroy();
 
         // Terminate GLFW and free the error callback
         glfwTerminate();
--- src/main/java/org/almostcraft/core/Window.java
+++ src/main/java/org/almostcraft/core/Window.java
@@ -1,5 +1,113 @@
 package org.almostcraft.core;
 
-public class Window {
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryStack;
 
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.opengl.GL11.glViewport;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private long handle;
+
+    private int width;
+    private int height;
+    private String title;
+
+    public Window(int width, int height, String title) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Invalid window dimensions");
+        }
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be null or empty");
+        }
+
+        this.width = width;
+        this.height = height;
+        this.title = title;
+
+        create();
+    }
+
+    private void create() {
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        handle = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (handle == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        glfwSetFramebufferSizeCallback(handle, (window, w, h) -> {
+            this.width = w;
+            this.height = h;
+            glViewport(0, 0, w, h);
+        });
+    }
+
+    public long getHandle() {
+        return handle;
+    }
+
+    public void centerOnScreen() {
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(handle, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    handle,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            ); // the stack frame is popped automatically
+        }
+    }
+
+    public void makeContextCurrent() {
+        glfwMakeContextCurrent(handle);
+    }
+
+    public void enableVsync() {
+        glfwSwapInterval(1);
+    }
+
+    public void show() {
+        glfwShowWindow(handle);
+    }
+
+    public void destroy() {
+        glfwFreeCallbacks(handle);
+        glfwDestroyWindow(handle);
+    }
+
+    public boolean shouldClose() {
+        return glfwWindowShouldClose(handle);
+    }
+
+    public void swapBuffers() {
+        glfwSwapBuffers(handle);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
